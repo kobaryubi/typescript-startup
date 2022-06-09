7309bc99d25862310196392081fc7fe6bffb5d4d
koba <koba@growal.com>
2022-06-10 08:07:20 +0900

    feat: partial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 console.log('Hello TypeScript!')
+
+interface Todo {
+  title: string;
+  description: string;
+}
+
+const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>) => {
+  return { ...todo, ...fieldsToUpdate }
+}
+
+const todo = {
+  title: "organize desk",
+  description: "clear cluster",
+}
+
+const updatedTodo = updateTodo(todo, { description: "throw out trash" })
