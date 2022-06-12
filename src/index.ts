1e756ed692a5716b5bcf653eac56a610af8242d4
koba <koba@growal.com>
2022-06-12 11:10:57 +0900

    feat: record

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,16 @@ const readOnlyTodo: Readonly<Todo> = {
   title: "Delete inactive users",
   description: ""
 }
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+type CatName = "miffy" | "boris" | "mordred";
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" }
+}
